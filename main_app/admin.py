3a6236f1c96be5ecb608366a59ed13a5216a1f90
NatNichols <natalie.e.nichols@gmail.com>
2024-03-04 12:12:05 -0500

    register Photo model in admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 # imports
 from django.contrib import admin
-from .models import Show, Episode
+from .models import Show, Episode, Photo
 
 # registered models
 admin.site.register(Show)
 admin.site.register(Episode)
+admin.site.register(Photo)
