32c96792d79204fd35b2bb59d5ee5baec9e65d96
NatNichols <natalie.e.nichols@gmail.com>
2024-03-03 21:49:43 -0500

    define about URL and view function

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About my TV Collection</h1>')
