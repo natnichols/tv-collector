04252d89c4dffbbc32d867b6dcdbca94a5083f6c
NatNichols <natalie.e.nichols@gmail.com>
2024-03-05 16:51:36 -0500

    specify logout redirect URL

--- tvcollector/settings.py
+++ tvcollector/settings.py
@@ -136,6 +136,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'show-index'
 
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
